53d02df62dc300c355135d64658556a099e929cd
Arunima Chaudhuri <arunimachaudhuri2020@gmail.com>
2022-10-05 11:29:36 +0530

    displayUserInfo()

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@ let employeesArr = [];
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
+//Get and display 12 random users
+//1. Fetch function
 function fetchData(url) {
     return fetch(url)
         .then(response => response.json())
@@ -21,7 +23,28 @@ fetchData("https://randomuser.me/api/?results=12")
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
-
+/**
+ * Create, gallery items
+ * and modal to function as main components of app 
+ */
+//2. Gallery Items
+const displayUserInfo = (data) => {
+    const users =  data.forEach((user, index) => {
+           const html = `
+           <div class="card" data-index=${index}>
+           <div class="card-img-container">
+               <img class="card-img" src="${user.picture.large}" alt="profile picture">
+           </div>
+           <div class="card-info-container">
+               <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+               <p class="card-text">${user.email}</p>
+               <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+           </div>
+           </div>
+             `;
+        containerDiv.insertAdjacentHTML("beforeend", html);
+    })
+}
 
 
 // ------------------------------------------
